edc03110d4baaf0955946f894a17add03fcfa51d
MATT BRUTON <the.bruton@gmail.com>
2016-03-23 18:04:50 -0500

    changed event listener on delete button so edit button can function as intended. still need to make edit button actually modify existing element instead of making a new one

--- iife3_delete.js
+++ iife3_delete.js
@@ -1,6 +1,8 @@
 var Chatty = (function (originalChatty) {
   originalChatty.deleteButton= function(event){
+    if(event.target.id ==="delete") {
     this.removeChild(event.target.parentNode);
   }
+}
   return originalChatty;
 })(Chatty||{});
